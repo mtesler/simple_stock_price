8b039970370f84e5e5a8e6c924c90f2938adabd1
Michal Tesler <michal.tesler@gmail.com>
2022-08-29 16:40:25 +0200

    Downloaded historical data for selected company in defined time

--- simple_stock_price.py
+++ simple_stock_price.py
@@ -14,3 +14,7 @@ ticker_symbol = 'GOOGL'
 
 # get data on this ticker
 ticker_data = yf.Ticker(ticker_symbol)
+
+# get the historical prices for this ticker
+ticker_df = ticker_data.history(
+    period='1d', start='2020-01-01', end='2022-08-29')
