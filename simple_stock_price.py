1ca63fc2a04b5c54b8f25dfe541f29a96f3b052b
Michal Tesler <michal.tesler@gmail.com>
2022-08-29 16:41:25 +0200

    Added line charts

--- simple_stock_price.py
+++ simple_stock_price.py
@@ -18,3 +18,7 @@ ticker_data = yf.Ticker(ticker_symbol)
 # get the historical prices for this ticker
 ticker_df = ticker_data.history(
     period='1d', start='2020-01-01', end='2022-08-29')
+
+# create line charts
+st.line_chart(ticker_df.Close)
+st.line_chart(ticker_df.Volume)
